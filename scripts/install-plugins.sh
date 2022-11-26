12615f7685c4a0f0c89ef4850729cf5b0a988eb1
Matt Fellows <matt.fellows@onegeek.com.au>
2022-11-26 13:25:00 +1100

    chore: install plugins for build

---
+++ scripts/install-plugins.sh
@@ -0,0 +1,44 @@
+#!/bin/bash -e
+#
+# Usage:
+#   $ curl -fsSL https://raw.githubusercontent.com/pact-foundation/pact-plugins/master/install-cli.sh | bash
+# or
+#   $ wget -q https://raw.githubusercontent.com/pact-foundation/pact-plugins/master/install-cli.sh -O- | bash
+#
+
+function detect_osarch() {
+    case $(uname -sm) in
+        'Linux x86_64')
+            os='linux'
+            arch='x86_64'
+            ;;
+        'Darwin x86' | 'Darwin x86_64')
+            os='osx'
+            arch='x86_64'
+            ;;
+        'Darwin arm64')
+            os='osx'
+            arch='aarch64'
+            ;;
+        *)
+        echo "Sorry, you'll need to install the plugin CLI manually."
+        exit 1
+            ;;
+    esac
+}
+
+package=$(curl -s https://api.github.com/repos/pact-foundation/pact-plugins/releases | grep pact-plugin-cli | grep tag_name | head -n1 | egrep -o "pact-plugin-cli-v.[0-9\.]+")
+detect_osarch
+
+if [ ! -f ~/.pact/bin/pact-plugin-cli ]; then
+    echo "--- 🐿  Installing plugins CLI tool"
+    mkdir -p ~/.pact/bin
+    wget https://github.com/pact-foundation/pact-plugins/releases/download/${package}/pact-plugin-cli-${os}-${arch}.gz -O ~/.pact/bin/pact-plugin-cli-${os}-${arch}.gz
+    gunzip -N ~/.pact/bin/pact-plugin-cli-${os}-${arch}.gz
+    chmod +x ~/.pact/bin/pact-plugin-cli
+fi
+
+if [ ! -d ~/.pact/plugins/matt-0.0.3 ]; then
+    echo "--- 🐿  Installing MATT plugin"
+  ~/.pact/bin/pact-plugin-cli -y install https://github.com/mefellows/pact-matt-plugin/releases/tag/v0.0.3
+fi
